229fcb7ad5bf54eaa82bed004df80c0c848c57c3
rishi1717 <rshrrrss@gmail.com>
2022-03-25 19:16:48 +0530

    model updated

--- model/user.model.js
+++ model/user.model.js
@@ -4,17 +4,23 @@ let userSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: "Required",
+		trim: true,
+		minlength: [2, "Atleast 2 character required"],
 	},
 	userName: {
 		type: String,
 		required: "Required",
+		trim: true,
 		unique: true,
+		minlength: [5, "Atleast 5 character required"],
 	},
 	password: {
 		type: String,
+		minlength: [6, "Atleast 6 character required"],
 	},
 	email: {
 		type: String,
+		trim: true,
 		unique: true,
 	},
 })
